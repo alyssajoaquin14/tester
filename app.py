4e8fec1aedded3f6cc63f5dbd5164498f1d6ac50
Alyssa Joaquin <alyssa.joaquin@gmail.com>
2023-12-12 11:19:05 -0500

    initial code for app

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 # Sample app file
+
+def add_numbers(a, b):
+    return a + b
+
+def multiply_numbers(a, b):
+    return a * b
+
+def main():
+    x = 5
+    y = 3
+    sum_result = add_numbers(x, y)
+    product_result = multiply_numbers(x, y)
+
+    print(f"Sum: {sum_result}")
+    print(f"Product: {product_result}")
+
+if __name__ == "__main__":
+    main()
